1475f95ff842870a6c88932460c4deef07fa67ad
aloegartenn <97531085+aloegartenn@users.noreply.github.com>
2022-02-16 13:35:10 +0200

    s

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+let n = 1000;
+
+for (let i = 2; i <= n; i++) {
+    let flag = 1;
+    if (i > 2 && i % 2 != 0)
+    {
+        for (let j = 3; j*j <= i ; j=j+2)
+        {
+            if (i%j==0)
+            {
+                flag=0;
+                break;
+            }
+        }
+    }
+    else if (i != 2) flag = 0;
+    if (flag==1) {console.log(i);}
+}
